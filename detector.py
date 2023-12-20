5c702e48059bd1892eda71f0ba955c4749281d4d
Sunasuna24 <sunasunayaka1218@gmail.com>
2023-12-20 23:28:38 +0900

    画像に描画するための準備をした

--- detector.py
+++ detector.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import face_recognition
 import pickle
 from collections import Counter
+from PIL import Image, ImageDraw
 
 DEFAULT_ENCODINGS_PATH = Path("output/endodings.pkl")
 
@@ -37,11 +38,17 @@ def recognize_faces(image_location: str, model: str = "hog", encodings_location:
     input_face_locations = face_recognition.face_locations(input_image, model=model)
     input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
 
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+
     for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
-        print(name, bounding_box)
+        _display_face(draw, bounding_box, name)
+
+    del draw
+    pillow_image.show()
 
 def _recognize_face(unknown_encoding, loaded_encodings):
     boolean_matches = face_recognition.compare_faces(loaded_encodings["encodings"], unknown_encoding)

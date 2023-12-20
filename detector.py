8d5a31783bd43678368ae3e02764247909d19e03
Sunasuna24 <sunasunayaka1218@gmail.com>
2023-12-20 12:49:20 +0900

    エンコードを開けて、未ラベルの画像を読み込んだ

--- detector.py
+++ detector.py
@@ -27,4 +27,10 @@ def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_EN
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
-encode_known_faces()
+def recognize_faces(image_location: str, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
+
+# encode_known_faces()

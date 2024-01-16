4d9bc3a63a13bccce23fc982fb3a72666382bf2e
Sunasuna24 <sunasunayaka1218@gmail.com>
2024-01-16 20:03:08 +0900

    コマンドラインを追加した

--- detector.py
+++ detector.py
@@ -3,6 +3,7 @@ import face_recognition
 import pickle
 from collections import Counter
 from PIL import Image, ImageDraw
+import argparse
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 BOUNDING_BOX_COLOR = "blue"
@@ -11,6 +12,15 @@ TEXT_COLOR = "white"
 Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
+
+parser = argparse.ArgumentParser(description="Recognize faces in an image")
+parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument("--validate", action="store_true", help="Validate trained model")
+parser.add_argument("--test", action="store_true", help="Test the model with an unknown image")
+parser.add_argument("-m", action="store", default="hog", choices=["hog", "cnn"], help="Which model to use for training: hog (CPU), cnn (GPU)")
+parser.add_argument("-f", action="store", help="Path to an image with an unknown face")
+
+args = parser.parse_args()
 
 
 def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> None:
@@ -76,4 +86,12 @@ def validate(model: str = "hog"):
             recognize_faces(image_location=str(filepath.absolute()), model=model)
 
 # recognize_faces("unknown/unknown.jpg")
-validate()
+# validate()
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)

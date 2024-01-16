c89b69de7af2488228b4c679f2029d4ae5153d92
Sunasuna24 <sunasunayaka1218@gmail.com>
2024-01-16 18:40:54 +0900

    recognize_faces関数に説明を加えた

--- detector.py
+++ detector.py
@@ -44,29 +44,48 @@ def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_EN
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
+"""recognize_faces関数
+ラベル付けされていない画像の顔を識別することができる関数。
+
+引数1: string、識別したい未ラベルの画像のパス
+引数2: string、モデルの方。HOG
+引数3: エンコーディングした情報を格納するパス。DEFAULT_ENCODINGS_PATH
+"""
 def recognize_faces(image_location: str, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> None:
+    # ファイルに保存したエンコーディングを開ける
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
 
-    input_image = face_recognition.load_image_file(image_location)
-    input_face_locations = face_recognition.face_locations(input_image, model=model)
-    input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
+    input_image = face_recognition.load_image_file(image_location)                              # 入力した未ラベルの画像を読み込ませる
+    input_face_locations = face_recognition.face_locations(input_image, model=model)            # 未ラベルの画像の顔を検出する
+    input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)   # 未ラベルの画像の顔のエンコーディングを取得する
 
     pillow_image = Image.fromarray(input_image)
     draw = ImageDraw.Draw(pillow_image)
 
+    # 入力した未ラベルの画像を（for文を通して）過去に作成したエンコーディングの塊に晒して比較する。
     for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
         name = _recognize_face(unknown_encoding, loaded_encodings)
+        # 識別がどのモデルとも一致しない場合は"Unknown"という名前を付ける
         if not name:
             name = "Unknown"
+
+        # bouding_boxと一緒に検出したモデルをディスプレイに出力する
         _display_face(draw, bounding_box, name)
 
     del draw
     pillow_image.show()
 
+"""_recognize_face関数
+未ラベルの画像とモデルの両方のエンコーディングを比較して、（もしあれば）一致するモデルを返す
+
+引数1: 未ラベルの画像のエンコーディング
+引数2: モデルのエンコーディング
+"""
 def _recognize_face(unknown_encoding, loaded_encodings):
     boolean_matches = face_recognition.compare_faces(loaded_encodings["encodings"], unknown_encoding)
 
+    # Counterを用いて、よりマッチするモデルを選択する
     votes = Counter(
         name
         for match, name in zip(boolean_matches, loaded_encodings["names"])
